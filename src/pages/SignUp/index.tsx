66a54a613f618ad5f93af5c5d76d2e6a02d70adc
Victoria Palilingan <victoriapalilingan19@gmail.com>
2025-11-02 16:21:59 +0800

    create branch development

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,37 +1,8 @@
 import {StyleSheet, View} from 'react-native';
 import React from 'react';
-import Header from '../../components/molecules/Header';
-import TextInput from '../../components/molecules/TextInput';
-import Button from '../../components/atoms/Button';
-import Gap from '../../components/atoms/Gap';
 
-const SignUp = ({navigation}) => {
-  return (
-    <View style={styles.container}>
-      {/* Header dengan tombol panah */}
-      <Header label="Sign Up" onPress={() => navigation.goBack()} />
-
-      <View style={styles.contentWrapper}>
-        <TextInput label="Full Name" placeholder="Type your full name" />
-        <Gap height={16} />
-        <TextInput
-          label="Email Address"
-          placeholder="Type your email address"
-        />
-        <Gap height={16} />
-        <TextInput
-          label="Password"
-          placeholder="Type your password"
-          secureTextEntry
-        />
-        <Gap height={24} />
-        <Button
-          label="Continue"
-          onPress={() => console.log('Sign Up pressed')}
-        />
-      </View>
-    </View>
-  );
+const SignUp = ({}) => {
+  return <View style={styles.container}></View>;
 };
 
 export default SignUp;
