3d1731a24606f3f1b6ddbe324f195eee5b98c4fd
Newguinea Pangalila <s22310098@student.unklab.ac.id>
2025-11-02 18:09:44 +0800

    Tambah Text Input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,8 @@
 import {StyleSheet, View, Image, TouchableOpacity} from 'react-native';
 import React, {useState} from 'react';
 import Header from '../../components/molecules/Header';
+import TextInput from '../../components/molecules/TextInput';
+import Gap from '../../components/atoms/Gap';
 
 const SignUp = ({navigation}) => {
   const [photo, setPhoto] = useState(require('../../assets/null-photo.png'));
@@ -20,6 +22,21 @@ const SignUp = ({navigation}) => {
         <TouchableOpacity onPress={handleChoosePhoto} activeOpacity={0.7}>
           <Image source={photo} style={styles.photo} />
         </TouchableOpacity>
+      </View>
+
+      <View style={styles.contentWrapper}>
+        <TextInput label="Full Name" placeholder="Type your full name" />
+        <Gap height={16} />
+        <TextInput
+          label="Email Address"
+          placeholder="Type your email address"
+        />
+        <Gap height={16} />
+        <TextInput
+          label="Password"
+          placeholder="Type your password"
+          secureTextEntry
+        />
       </View>
     </View>
   );
