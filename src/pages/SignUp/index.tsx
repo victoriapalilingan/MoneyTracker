b663557029cbab2aba9b73a427afd0a1664988f8
Victoria Palilingan <victoriapalilingan19@gmail.com>
2025-11-02 19:06:03 +0800

    main

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,27 +1,8 @@
-import {StyleSheet, View, Image, TouchableOpacity} from 'react-native';
-import React, {useState} from 'react';
-import Header from '../../components/molecules/Header';
+import {StyleSheet, View} from 'react-native';
+import React from 'react';
 
-const SignUp = ({navigation}) => {
-  const [photo, setPhoto] = useState(require('../../assets/null-photo.png'));
-
-  const handleChoosePhoto = () => {
-    console.log('Change photo pressed');
-    // nanti bisa ditambahkan fitur pilih foto dari galeri
-  };
-
-  return (
-    <View style={styles.container}>
-      <Header label="Sign Up" onPress={() => navigation.goBack()} />
-
-      {/* Foto Profil Bulat di Tengah */}
-      <View style={styles.photoWrapper}>
-        <TouchableOpacity onPress={handleChoosePhoto} activeOpacity={0.7}>
-          <Image source={photo} style={styles.photo} />
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+const SignUp = () => {
+  return <View style={styles.container}>{}</View>;
 };
 
 export default SignUp;
@@ -30,22 +11,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
-  },
-  photoWrapper: {
-    alignItems: 'center',
-    marginTop: 24,
-  },
-  photo: {
-    width: 100,
-    height: 100,
-    borderRadius: 50, // bulat sempurna
-    borderWidth: 2,
-    borderColor: '#ffffffff',
-  },
-  contentWrapper: {
-    marginTop: 24,
-    flex: 1,
-    paddingHorizontal: 24,
-    paddingTop: 26,
   },
 });
